01525bca5aca4d790fcbb7db11653dba607546b2
palm000palmovich <ivankiselev6534@gmail.com>
2024-11-30 19:08:19 +0300

    start

---
+++ src/main/java/ru/hogwarts/schoolloohcs/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package ru.hogwarts.schoolloohcs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import ru.hogwarts.schoolloohcs.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    //
+}
