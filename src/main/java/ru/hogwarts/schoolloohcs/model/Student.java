192c0665b45bf581f1f43ebb1d83c733f9c157da
Palm_***_Palmovich <160827177+palm000palmovich@users.noreply.github.com>
2024-12-10 16:19:31 +0300

    Update Student.java

--- src/main/java/ru/hogwarts/schoolloohcs/model/Student.java
+++ src/main/java/ru/hogwarts/schoolloohcs/model/Student.java
@@ -15,7 +15,7 @@ public class Student {
     private String name;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "faculty_id")
     private Faculty faculty;
     public Student(){}
